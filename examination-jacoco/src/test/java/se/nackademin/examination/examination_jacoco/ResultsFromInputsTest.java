0e468d1406f09be5f2b5f2869bb4d5463bd50e92
SenayM <mailtosenay@gmail.com>
2016-10-21 22:27:18 +0200

    After 100 pecent on ResultsFromInput

--- examination-jacoco/src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ examination-jacoco/src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -6,30 +6,4 @@ import org.junit.Test;
 
 public class ResultsFromInputsTest {
 
-	@Test
-	public void testNameLengthGettersAndSettersMethods() {
-		
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForNameLenght(10);
-		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
-	}
-	@Test
-	public void testGenderGettersAndSettersMethods() {
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForGender('M');
-		assertEquals("The result should be M", resultsFromInputs.getResultForGender(), 'M');
-	}
-	@Test
-	public void testAgeGettersAndSettersMethods() {
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForAge(25); 
-		assertEquals("The result should be 25", resultsFromInputs.getResultForAge() ,25);
-	}
-	@Test
-	public void testHomeCityGettersAndSettersMethods() {
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForHomeCity(2); 
-		assertEquals("The result should be 2", resultsFromInputs.getResultForHomeCity() ,2);
-	}
-
 }
